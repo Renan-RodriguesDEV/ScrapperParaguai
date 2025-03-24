c0fa34d6970bdd84ad933f7081208c868733a8ae
Renan-RodriguesDEV <renanrodrigues7110@gmail.com>
2025-03-24 14:50:23 -0300

    pip i

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import threading
 import time
@@ -5,9 +6,10 @@ import time
 
 # Execute a Python script
 def execute(path):
-    subprocess.run(["Python", path])
+    subprocess.run(["python3", path])
 
 
+os.sys("playwright install")
 # Execute a Python script
 thred1 = threading.Thread(target=execute, args=("scrappers/scrapper_cotacao.py",))
 thred1.start()
