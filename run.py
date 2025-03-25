25f237b1c8e5abe1131a0a8b18a64dcf220cf6c1
Renan-RodriguesDEV <renanrodrigues7110@gmail.com>
2025-03-25 12:08:42 -0300

    fix: args params

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
 import os
 import subprocess
+import sys
 import threading
 import time
 
 
 # Execute a Python script
 def execute(path):
-    subprocess.run(["python3", path])
+    subprocess.run(["python", path])
 
 
-try:
-    os.system("playwright install")
-except Exception as e:
-    print(e)
-    pass
+if len(sys.argv) > 1:
+    if sys.argv[1] == "install" or sys.argv[1] == "i":
+        try:
+            os.system("playwright install chromium")
+        except Exception as e:
+            print(e)
+            pass
+
 
 # Execute a Python script
 thred1 = threading.Thread(target=execute, args=("scrappers/scrapper_cotacao.py",))
